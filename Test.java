a9ab1fbf0e0f283d6693edf08793572023e6838a
Kevin C <48696458+kcruzs@users.noreply.github.com>
2023-03-18 00:18:31 -0600

    add Lienzo y contralador

--- Test.java
+++ Test.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import javax.swing.JFrame;
+
 public class Test {
     public static void main(String[] args) {
         String expresion = null;                
@@ -14,8 +16,14 @@ public class Test {
         }
         Arbol arbol = new Arbol(expresion);
         Lienzo lienzo = new Lienzo();
+        Controlador objControlador = new Controlador(lienzo, arbol);
 
-        lienzo.setObjArbol(arbol);
-        
+        objControlador.iniciar();
+        JFrame ventana = new JFrame();
+        ventana.getContentPane().add(lienzo);
+        ventana.setDefaultCloseOperation(3);
+        ventana.setSize(600, 600);
+        ventana.setVisible(true);
+
     }
 }
